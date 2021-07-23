afc4698646c8711f5b94a083eb35c3e980dea1b2
Co1lin <mail@co1in.me>
2021-07-23 17:28:49 +0800

    init drnet

---
+++ models/drnet.py
@@ -0,0 +1,24 @@
+import torch
+import pytorch_lightning as pl
+
+class DRNet(pl.LightningModule):
+
+    def __init__(self):
+
+        pass
+
+    def forward(self, x):
+
+        pass
+
+    def training_step(self, batch, batch_idx):
+        
+        pass
+
+    def validation_step(self, batch, batch_idx):
+
+        pass
+
+    def configure_optimizers(self):
+
+        pass
