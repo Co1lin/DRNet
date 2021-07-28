b8e1f5e9a1b2d2934fede01ac99ee5eba89fc243
Co1lin <mail@co1in.me>
2021-07-28 12:53:39 +0800

    fix resume from checkpoint

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ import numpy as np
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.trainer.trainer import Trainer
 from pytorch_lightning.plugins import DDPPlugin
 import torch
+from torch._C import Value
 from data.dr_datamodule import DRDataModule
 from models.drnet import DRNet
 
-def init_wandb():
+def init_wandb(cfg):
     os.environ['WANDB_API_KEY'] = 'a5044d4b533063065587a9fce532ad394071fc48'
     wandb_logger = WandbLogger(
         entity='co1lin',
         project='DRNet',
+        group=cfg.task.name,
     )
     return wandb_logger
 
@@ -26,40 +27,44 @@ ckpt_callback_list: list = None
 output_dir: str = None
 
 def train(cfg):
+    ckpt_callback_list = [
+        ModelCheckpoint(
+            monitor='val_loss',
+            dirpath=output_dir,
+            filename=f'{cfg.task.name}-' + '{epoch}-{val_loss:.2f}-best',
+            save_top_k=3,
+            save_last=True,
+            mode='min',
+        ),
+    ]
+    wandb_logger = init_wandb(cfg)
     if not cfg.task.resume:
-        ckpt_callback_list = [
-            ModelCheckpoint(
-                monitor='val_loss',
-                dirpath=output_dir,
-                filename=f'{cfg.task.name}-' + '{epoch}-{val_loss:.2f}-best',
-                save_top_k=3,
-                mode='min',
-            ),
-            ModelCheckpoint(
-                dirpath=output_dir,
-                filename=f'{cfg.task.name}-' + '{epoch}-{val_loss:.2f}',
-            )
-        ]
-        wandb_logger = init_wandb()
         trainer = pl.Trainer(
             max_epochs=cfg.task.epochs,
             gpus=cfg.gpus, 
             logger=wandb_logger, 
             accelerator='ddp', 
-            log_every_n_steps=20, 
+            log_every_n_steps=10, 
             callbacks=ckpt_callback_list,
             #plugins=DDPPlugin(find_unused_parameters=False),
         )
-        trainer.fit(model, datamodule=datamodule)
     else:
-        trainer = Trainer(
+        trainer = pl.Trainer(
             resume_from_checkpoint=cfg.weight,
+            max_epochs=cfg.task.epochs,
+            gpus=cfg.gpus,
+            logger=wandb_logger, 
+            accelerator='ddp', 
+            log_every_n_steps=10, 
+            callbacks=ckpt_callback_list,
+            #plugins=DDPPlugin(find_unused_parameters=False),
         )
-        trainer.fit(model)
+    # end if
+    trainer.fit(model, datamodule=datamodule)
 
 def test(cfg):
     trainer = pl.Trainer(
-        gpus=cfg.gpus,
+        gpus=cfg.gpus[0],
     )
     trainer.test(model, datamodule=datamodule)
 
@@ -79,21 +84,23 @@ def main(cfg):
     global ckpt_callback_list
     global output_dir
 
-    output_dir = os.path.abspath(os.curdir)
-    os.chdir(hydra.utils.get_original_cwd())
+    output_dir = os.path.abspath(os.curdir) # get hydra output dir
+    os.chdir(hydra.utils.get_original_cwd())    # set working dir to the original one
 
     seed_all(cfg.seed)
     
     if cfg.task.mode == 'train':
-        if not cfg.task.resume:
+        if cfg.task.resume:
+            model = DRNet(cfg)  # DRNet.load_from_checkpoint(cfg.weight)
+        else:
             if not cfg.task.finetune or not os.path.exists(cfg.weight):
-                model = DRNet(cfg)
+                model = DRNet(cfg)  # train from scratch
             else:   # finetuning based on previous weights
                 model = DRNet.load_from_checkpoint(cfg.weight, config=cfg)
-        else:   # resume
-            model = DRNet(cfg) # DRNet.load_from_checkpoint(cfg.weight)
-    else:
+    elif cfg.task.mode == 'test':
         model = DRNet.load_from_checkpoint(cfg.weight, config=cfg)
+    else:
+        raise ValueError(f'Invalid task mode: {cfg.task.mode}')
     
     datamodule = DRDataModule(cfg)
 
@@ -106,4 +113,4 @@ def main(cfg):
 if __name__ == '__main__':
     main()
 
-# python main.py task=train_comp 'gpus=[0,1,2,7]'
+# python main.py task=train_comp gpus='[0,1,2,7]'
